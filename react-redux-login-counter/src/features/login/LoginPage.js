7bc435769840437a4523f87dec4574323ee0321d
U Senarathna <2udeshika@gmail.com>
2024-02-19 23:27:53 +1300

    CFT Login

--- react-redux-login-counter/src/features/login/LoginPage.js
+++ react-redux-login-counter/src/features/login/LoginPage.js
@@ -29,7 +29,7 @@ const LoginPage = () => {
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-              CFT LOgin
+              CFT Login
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
